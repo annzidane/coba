78a7bba5227121767ac2c540a3934e24aaed5e1d
Muhamad Aenun Zidane <111588431+annzidane@users.noreply.github.com>
2024-07-16 16:29:58 +0700

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -28,13 +28,13 @@ export default function About() {
         adalah aspek pemecahan masalah. Saya 
         <span className="font-medium"> sangat senang </span> 
         saat akhirnya menemukan solusi untuk suatu masalah. Tumpukan teknologi utama saya adalah 
-        <span className="font-medium"> React, Next.js, Laravel, dan Mysql</span>. 
+        <span className="font-medium"> React, Next.js, PHP, Laravel, dan Mysql</span>. 
         Saya selalu ingin belajar teknologi baru. Saat ini, saya sedang mencari 
         <span className="font-medium"> posisi penuh waktu</span> sebagai pengembang perangkat lunak.
       </p>
 
       <p>
-        <span className="italic">Saat tidak sedang coding</span>, saya menikmati bermain video game, menonton film, dan bermain dengan anjing saya. Saya juga suka <span className="font-medium">mempelajari hal-hal baru</span>. Saat ini, saya sedang belajar tentang <span className="font-medium">bahasa pemrograman lain</span>.
+        <span className="italic">Saat tidak sedang coding</span>, saya menikmati bermain video game, menonton film, dan Mendengarkan Musik. Saya juga suka <span className="font-medium">mempelajari hal-hal baru</span>. Saat ini, saya sedang belajar tentang <span className="font-medium">bahasa pemrograman lain</span>.
       </p>
     </motion.section>
   );
